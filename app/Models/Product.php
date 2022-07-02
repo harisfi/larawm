21b004fa9dd1f6466003573142ab72dc1ab809f7
Muhammad Haris Firmansyah <acc.cp.haris@gmail.com>
2022-07-02 22:12:33 +0700

    Update models and seeders

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public $incrementing = false;
+    protected $guarded = [];
+
+    public function warehouse()
+    {
+        return $this->belongsTo(Warehouse::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function($model) {
+            $model->id = Str::uuid();
+        });
+    }
 }
--- app/Models/Warehouse.php
+++ app/Models/Warehouse.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Warehouse extends Model
 {
     use HasFactory;
+
+    public $incrementing = false;
+    protected $guarded = [];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function($model) {
+            $model->id = Str::uuid();
+        });
+    }
 }
