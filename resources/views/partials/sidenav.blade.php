c3b2465a52c96f174710b4e2dc5da3f3f76eaced
Muhammad Haris Firmansyah <acc.cp.haris@gmail.com>
2022-07-03 11:42:56 +0700

    Add partials and layout

---
+++ resources/views/partials/sidenav.blade.php
@@ -0,0 +1,57 @@
+<aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
+    <div class="sidenav-header">
+        <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+        <div class="navbar-brand m-0 d-flex align-items-center">
+            <span class="navbar-brand-img">
+                <i class="ni ni-spaceship text-primary text-4xl opacity-10 me-3"></i>
+            </span>
+            <span class="ms-1 font-weight-bold text-lg">LaraWM</span>
+        </div>
+    </div>
+    <hr class="horizontal dark mt-0">
+    <div class="collapse navbar-collapse w-auto h-auto" id="sidenav-collapse-main">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link @if(request()->is('dashboard')) active @endif" href="{{ route('dashboard') }}">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Dashboard</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link @if(request()->is('warehouse*')) active @endif" href="{{ url('warehouse') }}">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-building text-success text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Warehouse</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link @if(request()->is('product*')) active @endif" href="{{ url('product') }}">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-app text-warning text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Product</span>
+                </a>
+            </li>
+            <hr class="horizontal dark">
+            <li class="nav-item">
+                <a class="nav-link @if(request()->is('profile*')) active @endif" href="{{ url('profile') }}">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Profile</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('logout') }}">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-button-power text-danger text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Sign Out</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</aside>
